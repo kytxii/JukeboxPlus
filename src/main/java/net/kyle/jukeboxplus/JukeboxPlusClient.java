3b4db07f5d890a3b2c0e44da29f08e786c1ddd0c
kytxii <tilleykyle12@gmail.com>
2026-05-09 20:51:56 -0700

    first commit

---
+++ src/main/java/net/kyle/jukeboxplus/JukeboxPlusClient.java
@@ -0,0 +1,10 @@
+package net.kyle.jukeboxplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JukeboxPlusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
