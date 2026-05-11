7bc5a0da1d195bc40c8bc726ac6600893cbbf85f
kytxii <tilleykyle12@gmail.com>
2026-05-11 12:23:00 -0700

    feat: add item group support for vanilla discs and mod discs

--- src/main/java/net/kyle/jukeboxplus/item/ModItemGroups.java
+++ src/main/java/net/kyle/jukeboxplus/item/ModItemGroups.java
@@ -4,19 +4,35 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.kyle.jukeboxplus.JukeboxPlus;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroups {
     public static final ItemGroup JUKEBOX_PLUS_GROUP = Registry.register(Registries.ITEM_GROUP,
         new Identifier(JukeboxPlus.MOD_ID, "jukebox_plus"),
-        FabricItemGroup.builder().displayName(Text.translatable("itemgroup.jukebox_plus"))
-            .icon(() -> new ItemStack(ModItems.JUKEBOX_PLUS)).entries((displayContext, entries) -> {
+        FabricItemGroup.builder()
+            .displayName(Text.translatable("itemgroup.jukebox_plus"))
+            .icon(() -> new ItemStack(ModItems.JUKEBOX_PLUS))
+            .entries((displayContext, entries) -> {
+                // Jukebox+
                 entries.add(ModItems.JUKEBOX_PLUS);
-                // Add discs
-            }).build());
+
+                // Vanilla (minecraft:music_discs)
+                Registries.ITEM.getEntryList(ItemTags.MUSIC_DISCS)
+                    .ifPresent(tag -> tag.forEach(e -> entries.add(e.value())));
+
+                // More Music Discs support
+                Registries.ITEM.getIds().stream()
+                    .filter(id -> id.getNamespace().equals("morediscs"))
+                    .map(Registries.ITEM::get)
+                    .filter(item -> item != Items.AIR)
+                    .forEach(entries::add);
+            })
+            .build());
 
     public static void registerItemGroups() {
         JukeboxPlus.LOGGER.info("Registering Item Groups for " + JukeboxPlus.MOD_ID);
