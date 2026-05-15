98d1b43d7eba2c4ca7860a3a335b7a7062486e5c
kytxii <tilleykyle12@gmail.com>
2026-05-14 23:24:15 -0700

    feat: add playback state fields and NBT persistence to block entity

--- src/main/java/net/kyle/jukeboxplus/block/entity/JukeboxPlusBlockEntity.java
+++ src/main/java/net/kyle/jukeboxplus/block/entity/JukeboxPlusBlockEntity.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventories;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.screen.NamedScreenHandlerFactory;
@@ -16,6 +17,18 @@ import net.minecraft.text.Text;
 
 public class JukeboxPlusBlockEntity extends BlockEntity implements Inventory, NamedScreenHandlerFactory {
     private final DefaultedList<ItemStack> items = DefaultedList.ofSize(9, ItemStack.EMPTY);
+
+    private int currentSlot = 0;
+    private int ticksPlayed = 0;
+    private int discDurationTicks = 0;
+    private boolean isPlaying = false;
+    private LoopMode loopMode = LoopMode.OFF;
+
+    public enum LoopMode { 
+        OFF, 
+        LOOP_ONE, 
+        LOOP_ALL 
+    }
 
     public JukeboxPlusBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.JUKEBOX_PLUS_BLOCK_ENTITY, pos, state);
@@ -72,4 +85,52 @@ public class JukeboxPlusBlockEntity extends BlockEntity implements Inventory, Na
     public ScreenHandler createMenu(int syncId, PlayerInventory playerInventory, PlayerEntity player) {
         return new JukeboxPlusScreenHandler(syncId, playerInventory, this);
     }
+
+    // NBT
+    @Override
+    public void writeNbt(NbtCompound nbt) {
+        Inventories.writeNbt(nbt, items);
+
+        nbt.putInt("currentSlot", currentSlot);
+        nbt.putInt("ticksPlayed", ticksPlayed);
+        nbt.putInt("discDurationTicks", discDurationTicks);
+        nbt.putBoolean("isPlaying", isPlaying);
+        nbt.putString("loopMode", loopMode.name());
+
+        super.writeNbt(nbt);
+    }
+
+    @Override
+    public void readNbt(NbtCompound nbt) {
+        super.readNbt(nbt);
+
+        Inventories.readNbt(nbt, items);
+
+        currentSlot = nbt.getInt("currentSlot");
+        ticksPlayed = nbt.getInt("ticksPlayed");
+        discDurationTicks = nbt.getInt("discDurationTicks");
+        isPlaying = nbt.getBoolean("isPlaying");
+
+        try {
+            loopMode = LoopMode.valueOf(nbt.getString("loopMode"));
+        } catch (IllegalArgumentException e) {
+            loopMode = LoopMode.OFF;
+        }
+    }
+
+    // Getters/setters
+    public int getCurrentSlot() { return currentSlot; }
+    public void setCurrentSlot(int slot) {currentSlot = slot; markDirty(); }
+
+    public int getTicksPlayed() { return ticksPlayed; }
+    public void setTicksPlayed(int ticks) { ticksPlayed = ticks; markDirty(); }
+
+    public int getDiscDurationTicks() { return discDurationTicks; }
+    public void setDiscDurationTicks(int ticks) { discDurationTicks = ticks; markDirty(); }
+
+    public boolean isPlaying() { return isPlaying; }
+    public void setPlaying(boolean playing) { isPlaying = playing; markDirty(); }
+
+    public LoopMode getLoopMode() { return loopMode; }
+    public void setLoopMode(LoopMode mode) { loopMode = mode; markDirty(); }
 }
