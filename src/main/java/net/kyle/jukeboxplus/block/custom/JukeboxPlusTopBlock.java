76a75ab679d779051d1a90540ddf7cde37a5051e
kytxii <tilleykyle12@gmail.com>
2026-05-20 22:52:17 -0700

    feat: add directional hitbox to jukebox block

--- src/main/java/net/kyle/jukeboxplus/block/custom/JukeboxPlusTopBlock.java
+++ src/main/java/net/kyle/jukeboxplus/block/custom/JukeboxPlusTopBlock.java
@@ -8,16 +8,21 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
 import net.kyle.jukeboxplus.block.ModBlocks;
 import net.kyle.jukeboxplus.block.entity.JukeboxPlusBlockEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.enums.DoubleBlockHalf;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.BlockView;
 
 public class JukeboxPlusTopBlock extends Block {
+    private static final VoxelShape SHAPE = Block.createCuboidShape(0, 0, 0, 16, 8, 16);
+
     public JukeboxPlusTopBlock(Settings settings) {
         super(settings);
         setDefaultState(getStateManager().getDefaultState()
@@ -60,5 +65,10 @@ public class JukeboxPlusTopBlock extends Block {
             }
         }
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
     }
 }
